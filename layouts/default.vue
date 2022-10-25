17afd472140bb359b961955c2fe343fc60a32f6f
tarenadiyaksa <tarenadiyaksa@gmail.com>
2022-10-25 20:42:34 +0700

    Add: footer | Fix: Navbar

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Navbar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar.vue";
+export default { components: { Navbar } };
+</script>
+
+<style lang="scss" scoped></style>
